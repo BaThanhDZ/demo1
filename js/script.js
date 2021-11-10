25d37b45270a0e85612688d38a1dc1e06f4cc917
TAYMONKHANHPC\WinX <dinhnguyen.bathanh@gmail.com>
2021-11-10 10:39:59 +0700

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function menuNav() {
+	$('.nav').fadeToggle('nav_hide');
+}
+
+function menuDetail() {
+	$('.detail_2').toggleClass('detail_hide');
+}
+
+function menuDrop() {
+	$('.drop-down').toggleClass('drop-down_hide');
+}
